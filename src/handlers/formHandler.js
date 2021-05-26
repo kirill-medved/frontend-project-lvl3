c5bf857a859201322270f631f3457fb97c616d80
Kirill Medved <medved.kirill@bk.ru>
2021-05-27 00:38:03 +0300

    Fix17-3

--- src/handlers/formHandler.js
+++ src/handlers/formHandler.js
@@ -14,8 +14,7 @@ const formHandler = (e) => {
 
   const documentEl = document.querySelector('body');
   const divEl = document.createElement('div');
-  divEl.textContent =
-    'HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!';
+  divEl.textContent = `HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII!!${objData}`;
   documentEl.append(divEl);
   // const formData = new FormData(e.target);
   // const objData = Object.fromEntries(formData);
